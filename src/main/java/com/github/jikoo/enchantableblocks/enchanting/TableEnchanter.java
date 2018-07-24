c7b03d518a91831374b733e1d29021ffa8f88b65
Jikoo <Jikoo.Games@gmail.com>
2018-07-23 21:16:15 -0400

    Back to a single module project, dropping support for versions < 1.13
    CompatibilityUtil still offers, but things will actually break. Not ready for release yet.

---
+++ src/main/java/com/github/jikoo/enchantableblocks/enchanting/TableEnchanter.java
@@ -0,0 +1,149 @@
+package com.github.jikoo.enchantableblocks.enchanting;
+
+import com.github.jikoo.enchantableblocks.EnchantableBlocksPlugin;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentOffer;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Listener for handling enchanting in an enchantment table.
+ *
+ * @author Jikoo
+ */
+public class TableEnchanter implements Listener {
+
+	private final EnchantableBlocksPlugin plugin;
+	private final Map<UUID, Map<Material, Map<Integer, Map<Enchantment, Integer>>>> enchantmentOffers;
+	private final Map<UUID, Map<Material, Map<Integer, Integer[]>>> enchantmentOfferLevels;
+	private final Map<UUID, Integer[]> lastOffer;
+
+	public TableEnchanter(EnchantableBlocksPlugin plugin) {
+		this.plugin = plugin;
+		this.enchantmentOffers = new HashMap<>();
+		this.enchantmentOfferLevels = new HashMap<>();
+		this.lastOffer = new HashMap<>();
+	}
+
+	@EventHandler
+	public void onPrepareItemEnchant(final PrepareItemEnchantEvent event) {
+		this.lastOffer.remove(event.getEnchanter().getUniqueId());
+
+		if (event.getItem().getEnchantments().size() > 0
+				// TODO: enchantable materials list
+				|| !event.getItem().getType().equals(Material.FURNACE)
+				|| event.getItem().getAmount() != 1
+				|| this.plugin.getEnchantments().size() <= 0
+				// TODO: rework permissions
+				|| !event.getEnchanter().hasPermission("enchantableblocks.enchant.table")) {
+			return;
+		}
+
+		// Not normally enchantable, event must be un-cancelled.
+		event.setCancelled(false);
+
+		UUID uuid = event.getEnchanter().getUniqueId();
+		Material material = event.getItem().getType();
+
+		// Calculate levels offered for bookshelf count
+		this.enchantmentOfferLevels.compute(uuid, (id, materialOfferLevels) -> {
+			if (materialOfferLevels == null) {
+				materialOfferLevels = new HashMap<>();
+			}
+
+			materialOfferLevels.compute(event.getItem().getType(), (mat, buttonOfferLevels) -> {
+				if (buttonOfferLevels == null) {
+					buttonOfferLevels = new HashMap<>();
+				}
+
+				buttonOfferLevels.putIfAbsent(event.getEnchantmentBonus(),
+						EnchantmentUtil.getButtonLevels(event.getEnchantmentBonus()));
+
+				return buttonOfferLevels;
+			});
+
+			return materialOfferLevels;
+		});
+
+		// Calculate enchantments offered for levels offered
+		this.enchantmentOffers.compute(event.getEnchanter().getUniqueId(), (id, materialOffers) -> {
+			if (materialOffers == null) {
+				materialOffers = new HashMap<>();
+			}
+			materialOffers.compute(event.getItem().getType(), (mat, levelOffers) -> {
+				if (levelOffers == null) {
+					levelOffers = new HashMap<>();
+				}
+
+				Integer[] levels = enchantmentOfferLevels.get(uuid).get(material).get(event.getEnchantmentBonus());
+
+				// Set up EnchantmentOffers
+				for (int i = 0; i < event.getOffers().length; ++i) {
+					if (i >= levels.length) {
+						event.getOffers()[i] = null;
+						continue;
+					}
+
+					levelOffers.computeIfAbsent(levels[i], (level) ->
+							EnchantmentUtil.calculateFurnaceEnchants(this.plugin, level));
+
+					int buttonLevel = levels[i];
+					Map<Enchantment, Integer> enchantments = this.enchantmentOffers.get(uuid).get(material).get(buttonLevel);
+
+					if (enchantments.isEmpty() || levels[i] < 1) {
+						event.getOffers()[i] = null;
+						continue;
+					}
+
+					Map.Entry<Enchantment, Integer> firstEnchant = enchantments.entrySet().iterator().next();
+					event.getOffers()[i] = new EnchantmentOffer(firstEnchant.getKey(), firstEnchant.getValue(), buttonLevel);
+				}
+
+				this.lastOffer.put(uuid, levels);
+
+				return levelOffers;
+			});
+
+			return materialOffers;
+		});
+
+	}
+
+
+	@EventHandler
+	public void onEnchantItem(final EnchantItemEvent event) {
+
+		// TODO: all of the above
+		if (event.getItem().getType() != Material.FURNACE
+				|| event.getItem().getAmount() != 1
+				|| !event.getEnchanter().hasPermission("enchantableblocks.enchant.table")
+				|| !this.lastOffer.containsKey(event.getEnchanter().getUniqueId())) {
+			return;
+		}
+
+		UUID uuid = event.getEnchanter().getUniqueId();
+		int level = this.lastOffer.remove(uuid)[event.getExpLevelCost()];
+		Map<Enchantment, Integer> enchantments = this.enchantmentOffers.get(uuid).get(event.getItem().getType()).get(level);
+
+		event.getEnchantsToAdd().putAll(enchantments);
+	}
+
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+	public void onEnchantItemSucceed(final EnchantItemEvent event) {
+		// Player has attempted enchanting anything, all enchants are re-rolled.
+		UUID uuid = event.getEnchanter().getUniqueId();
+		this.enchantmentOfferLevels.remove(uuid);
+		this.enchantmentOffers.remove(uuid);
+		this.lastOffer.remove(uuid);
+	}
+
+}
---
+++ src/main/java/com/github/jikoo/enchantableblocks/listener/WorldListener.java
@@ -0,0 +1,68 @@
+package com.github.jikoo.enchantableblocks.listener;
+
+import com.github.jikoo.enchantableblocks.EnchantableBlocksPlugin;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.event.world.ChunkUnloadEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+
+/**
+ * Listener for generic world events.
+ *
+ * @author Jikoo
+ */
+public class WorldListener implements Listener {
+
+	private final EnchantableBlocksPlugin plugin;
+
+	public WorldListener(EnchantableBlocksPlugin plugin) {
+		this.plugin = plugin;
+		// TODO: Enable version-specific  block listener
+	}
+
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+	public void onChunkLoad(final ChunkLoadEvent event) {
+		this.plugin.loadChunkEnchantableBlocks(event.getChunk());
+	}
+
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+	public void onChunkUnload(final ChunkUnloadEvent event) {
+		this.plugin.unloadChunkEnchantableBlocks(event.getChunk());
+	}
+
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+	public void onBlockPlace(final BlockPlaceEvent event) {
+		this.plugin.createEnchantableBlock(event.getBlock(), event.getItemInHand());
+	}
+
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+	public void onBlockBreak(final BlockBreakEvent event) {
+		ItemStack itemStack = this.plugin.destroyEnchantableBlock(event.getBlock());
+
+		if (itemStack == null || !event.isDropItems()) {
+			return;
+		}
+
+		event.setDropItems(false);
+
+		// Schedule a task with no delay so block break completes before items are dropped - prevents weird ejections.
+		Player player = event.getPlayer();
+		if (player.getGameMode() != GameMode.CREATIVE
+				&& !event.getBlock().getDrops(event.getPlayer().getInventory().getItemInMainHand()).isEmpty()) {
+			new BukkitRunnable() {
+				@Override
+				public void run() {
+					event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), itemStack);
+				}
+			}.runTask(plugin);
+		}
+	}
+
+}
