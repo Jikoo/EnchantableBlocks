b093f29f8494391565dd66527c3fa4261fee779d
Jikoo <jikoo.games@gmail.com>
2021-06-15 08:04:58 -0400

    Add release script
    I am lazy

---
+++ scripts/tag_release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ ! $1 ]]; then
+  echo "Please provide a version string."
+  return
+fi
+
+version="$1"
+snapshot="${version%.*}.$((${version##*.} + 1))-SNAPSHOT"
+
+mvn versions:set -DnewVersion="$version"
+
+git add .
+git commit -S -m "Bump version to $version for release"
+git tag -s "$version" -m "Release $version"
+
+mvn clean package -am -P all
+
+mvn versions:set -DnewVersion="$snapshot"
+
+git add .
+git commit -S -m "Bump version to $snapshot for development"
